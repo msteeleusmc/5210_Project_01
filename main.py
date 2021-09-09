c95e3f3072f6974f8d83f04eecc78b4c2495d983
Michael <michaelsteele@my.unt.edu>
2021-09-08 21:33:24 -0500

    michael added a main function and matrix build function

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import numpy as np
 
-# Variable checks if the order is completed or not
-order_filled = False
-# Size of the array is established
-rows, cols = (6, 6)
-# Build the warehouse matrix 6 x 6
-matrix_arry = [[0]*cols]*rows
-#--matrix_arry = [[1,2,3,4,5,6], [7,8,9,10,11,12], [13,14,15,16,17,18],
-   #--            [19,20,21,22,23,24], [25,26,27,28,29,30], [31,32,33,34,35,36]]
+# -----------------------------------------------------------------------------------
+#                            Global Function
+# -----------------------------------------------------------------------------------
+def buildWareHouse(matrix_arry):
+    matrix_arry = [[1,2,3,4,5,6], [7,8,9,10,11,12], [13,14,15,16,17,18],
+                  [19,20,21,22,23,24], [25,26,27,28,29,30], [31,32,33,34,35,36]]
+    return matrix_arry
+
+# -----------------------------------------------------------------------------------
+#                         Executes Main Program
+# -----------------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Variable checks if the order is completed or not
+    order_filled = False
+    # Size of the array is established
+    rows, cols = (6, 6)
+    # Build the warehouse matrix 6 x 6
+    matrix_arry = [[0] * cols] * rows
+    matrix_arry = buildWareHouse(matrix_arry)
+
+    for row in matrix_arry:
+        print(row)
+
 
 # To search down add 6 to the array position
 # To search left subtract 1 to the array position
@@ -15,6 +30,4 @@ matrix_arry = [[0]*cols]*rows
 # To search up subtract 6 from the array
 # The above searches the neighborhood Up, Down, Left, Right
 
-for row in matrix_arry:
-    print(row)
 
